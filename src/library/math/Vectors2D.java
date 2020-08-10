2b8d57c64446dcb0643f27e465f8e7a91abdba1a
HaydenMarshalla <44349058+HaydenMarshalla@users.noreply.github.com>
2020-08-10 01:57:59 +0100

    Created a 2D vectors class

---
+++ src/library/math/Vectors2D.java
@@ -0,0 +1,129 @@
+package library.math;
+
+public class Vectors2D {
+    public double x;
+    public double y;
+
+    public Vectors2D() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Vectors2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vectors2D(Vectors2D vector) {
+        this.x = vector.x;
+        this.y = vector.y;
+    }
+
+    public Vectors2D set(double x, double y) {
+        this.x = x;
+        this.y = y;
+        return this;
+    }
+
+    public Vectors2D set(Vectors2D v1) {
+        this.x = v1.x;
+        this.y = v1.y;
+        return this;
+    }
+
+    public Vectors2D copy() {
+        return new Vectors2D(this.x, this.y);
+    }
+
+    public Vectors2D negative() {
+        this.x = -x;
+        this.y = -y;
+        return this;
+    }
+
+    public Vectors2D add(Vectors2D v) {
+        this.x = x + v.x;
+        this.y = y + v.y;
+        return this;
+    }
+
+    public Vectors2D addi(Vectors2D v) {
+        return new Vectors2D(x + v.x, y + v.y);
+    }
+
+    public Vectors2D normal() {
+        return new Vectors2D(-y, x);
+    }
+
+    public Vectors2D normalize() {
+        double d = Math.sqrt(x * x + y * y);
+
+        if (d == 0) {
+            d = 1;
+        }
+
+        this.x /= d;
+        this.y /= d;
+
+        return this;
+    }
+
+    public Vectors2D normalize(Vectors2D out) {
+        double d = Math.sqrt(x * x + y * y);
+
+        if (d == 0) {
+            d = 1;
+        }
+
+        out = new Vectors2D(x / d, y / d);
+
+        return out;
+    }
+
+    public double distance(Vectors2D v) {
+        double dx = this.x - v.x;
+        double dy = this.y - v.y;
+
+        return StrictMath.sqrt(dx * dx + dy * dy);
+    }
+
+    public Vectors2D subtract(Vectors2D v1) {
+        return new Vectors2D(this.x - v1.x, this.y - v1.y);
+    }
+
+    public double crossProduct(Vectors2D v1) {
+        return this.x * v1.y - this.y * v1.x;
+    }
+
+    public Vectors2D crossProduct(double a) {
+        return this.normal().scalar(a);
+    }
+
+    public Vectors2D scalar(double a) {
+        return new Vectors2D(x * a, y * a);
+    }
+
+    public double dotProduct(Vectors2D v1) {
+        return v1.x * this.x + v1.y * this.y;
+    }
+
+    public static Vectors2D cross(Vectors2D a, double s) {
+
+        return new Vectors2D(s * a.y, -s * a.x);
+
+    }
+
+    public static Vectors2D cross(double s, Vectors2D a) {
+
+        return new Vectors2D(-s * a.y, s * a.x);
+    }
+
+    public final boolean isValid() {
+        return !Double.isNaN(x) && !Double.isInfinite(x) && !Double.isNaN(y) && !Double.isInfinite(y);
+    }
+
+    @Override
+    public String toString() {
+        return this.x + " : " + this.y;
+    }
+}
